6b6301ccba1892687a5230b2a243f95d302179e5
itsvinayak <itssvinayak@gmail.com>
2020-04-09 15:36:18 +0530

    Updates

---
+++ precache-manifest.999137a85ebd9d0fb3a24ed6ffd7f90f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a9386de41f8bbdf19340f399739edc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee726199fa321c19562a",
+    "url": "/static/css/main.471ceea1.chunk.css"
+  },
+  {
+    "revision": "62417a6fced04793f6ff",
+    "url": "/static/js/2.de154dc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de154dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee726199fa321c19562a",
+    "url": "/static/js/main.2994463b.chunk.js"
+  },
+  {
+    "revision": "fae39dd47ad9a00bb02c",
+    "url": "/static/js/runtime-main.c24f648c.js"
+  }
+]);
