afe4ea98e8e9b6b69e7999ed92a61b46de047c07
vinayak <itssvinayak@gmail.com>
2023-12-09 03:28:47 +0530

    fixes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
   reactStrictMode: true,
   swcMinify: true,
 }
