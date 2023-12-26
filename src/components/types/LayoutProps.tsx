ed70a97f7807b5d97a40c9c94e3df71bf859fa0b
vinayak <itssvinayak@gmail.com>
2023-12-26 12:05:06 +0530

    clean up ...

--- src/components/types/LayoutProps.tsx
+++ src/components/types/LayoutProps.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 export interface LayoutProps {
   children: React.ReactNode;
   className?: string;
--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -2,13 +2,10 @@ import { FaSpinner } from 'react-icons/fa';
 
 const Loader = () => {
   return (
-    <div className='m-6 flex items-center justify-center w-screen h-screen overflow-hidden'>
+    <div className='m-6 flex h-screen w-screen items-center justify-center overflow-hidden'>
       <div className='flex w-full flex-col items-center justify-center gap-4'>
         <div className='flex h-28 w-28 animate-spin items-center justify-center rounded-full border-8 border-gray-300 text-4xl text-blue-400 dark:border-t-black'>
-          <FaSpinner 
-            className='animate-ping'
-            color='white'
-          />
+          <FaSpinner className='animate-ping' color='white' />
         </div>
       </div>
     </div>
