45691ac8129124cfbeb90e6749de2073d00bbe01
vinayak <itssvinayak@gmail.com>
2023-12-26 12:14:48 +0530

    PWA added

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 const { withContentlayer } = require('next-contentlayer');
 
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-};
+});
 
 module.exports = withContentlayer(nextConfig);
